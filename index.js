460a492664ae5f6a1250e8090fad97e41e3b99c2
Lfdstudios <54958946+Lfdstudios@users.noreply.github.com>
2021-03-17 15:43:07 -0400

    docker running

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
 const carquery = require('./inventorymethods');
 const userquery = require('./usermethods');
 const rulequery = require('./carrulesmethods');
