417bfb580420cc6738f8cb51f66a61092065ca51
Pronounced <jokerknight670@gmail.com>
2021-03-24 13:41:06 -0500

    Update connection.js

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const connection = "mongodb://localhost:27017/carDealership?authSource=admin";
 //const connection = "mongodb://192.168.1.65:27017/carDealership?authSource=admin";
+// const connection = "mongodb://mongo:27017/carDealership?authSource=admin";
 const connectDb = () => {
   return mongoose.connect(connection, {
     useNewUrlParser: true,
