b98089054a13756647113e71143eae0e2e47cb9b
Pronounced <jokerknight670@gmail.com>
2021-03-26 13:42:44 -0500

    moved fake data into own file. added test for inventory.

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3001;
 const cors = require('cors');
 const connectDb = require("./connection");
+var catalogRouter = require('./routes/catalog');
 app.use(cors());
 app.use(express.json());
-var catalogRouter = require('./routes/catalog');
 app.use('/', catalogRouter)
 
 app.listen(port, () => {
--- inventorymethod.test.js
+++ inventorymethod.test.js
@@ -1,25 +1,42 @@
 const express = require('express');
-const app = express(); // Link to your server file
+const app = express();
 const supertest = require("supertest");
 const request = supertest(app);
 const Inventory = require("./models/inventory.model.js");
 const mongoose = require("mongoose");
 const databaseName = "inventory_test";
 var catalogRouter = require('./routes/catalog');
+app.use(express.json());
 
 beforeAll(async (done) => {   
   app.use('/', catalogRouter)
-  const url = `mongodb://localhost/${databaseName}`;
-  //const url = `mongodb://localhost:27017/${databaseName}?authSource=admin`;
+  //const url = `mongodb://localhost/${databaseName}`;
+  const url = `mongodb://localhost:27017/${databaseName}?authSource=admin`;
   await mongoose.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    // user: "root",
-    // pass: "example",
+    user: "root",
+    pass: "example",
   });
 
   done();
 });
+
+it("Should get data from database", async (done) => {
+  const res = await request.post("/postcar").send({
+    vin: "String_vin",
+    year: 2020,
+    make: "str_make",
+    model: "srr_model",
+    seller: "str_seller",
+    color: "str_color",
+    isApproved: true,
+    image: "str_img",
+  });
+
+  expect(await request.get('/').then(res => res.body.length)).toBeGreaterThan(0);
+  done();
+})
 
 it("Should save user to database", async (done) => {
   const res = await request.post("/postcar").send({
@@ -34,14 +51,12 @@ it("Should save user to database", async (done) => {
   });
 
   expect(res.status).toBe(200);
-  jest.setTimeout(30000);
   done();
 });
 
 // Cleans up database between each test
 afterEach(async (done) => {
-  jest.setTimeout(30000);
-  await Inventory.deleteMany();
+  await Inventory.deleteMany({});
   done();
 });
 
