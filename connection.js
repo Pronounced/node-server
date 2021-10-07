714be77cab53d686e35a803a48f09832fad7b871
Jarrett Green <jarrett_green@outlook.com>
2021-10-07 17:45:57 -0500

    azure conn string

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 //const connection = "mongodb://mongo:27017/carDealership?authSource=admin";
-const connection = "mongodb://localhost:27017/carDealership?authSource=admin";
+const connection = "mongodb://pronounced:4qNPk2ZQKZ43jypX25DdoQU7BDbAgjm6iJ83peTwDOx5Hk84DhF8w5gCYBEMrNOhkz30c53Xnr4dpQnUOOAvlQ==@pronounced.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@pronounced@";
 const connectDb = () => {
   return mongoose.connect(connection, {
     useNewUrlParser: true,
